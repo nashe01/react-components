0ee4b51964981377e70ac735bc040d6144f49de1
Blessing Panashe Mupindu <127606157+nashe01@users.noreply.github.com>
2026-04-22 23:56:13 +0200

    add new component

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -1,12 +1,14 @@
 import AccordionCards from "./components/AccordionCards";
 import AchievementInfinityStrip from "./components/AchievementInfinityStrip";
 import AchievementStrip from "./components/AchievementStrip";
+import AchievementStripNoBorders from "./components/AchievementStripNoBorders";
 
 export default function App() {
   return (
     <main className="flex min-h-screen flex-col gap-10 bg-white p-6">
       <AccordionCards />
       <AchievementStrip />
+      <AchievementStripNoBorders />
       <AchievementInfinityStrip />
     </main>
   );
